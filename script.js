3a1ea3ff803a6605ea7c4466f7d66b003e79fdc6
MARTIN PONIACHIK <50235406@ort>
2026-05-28 09:01:18 -0300

    hemosavanzadomucho

---
+++ script.js
@@ -0,0 +1,13 @@
+  const inputNombre  = document.getElementById("nombre");
+  const inputEdad  = document.getElementById("edad");
+  const inputPresupuesto  = document.getElementById("presupuesto");
+  const selectDestino = document.getElementById("destino");
+  const divTemporada  = document.getElementById("temporada");
+  const divSeguro = document.getElementById("seguro");
+  const fbEdad = document.getElementById("fb-edad");
+  const fbNombre = document.getElementById("fb-nombre");
+  const fbPresupuesto = document.getElementById("fb-presupuesto");
+  const fbTemporada = document.getElementById("fb-temporada");
+  const divDestino  = document.getElementById("fb-destino");
+
+                        
